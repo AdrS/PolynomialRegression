35ba5a3b0e60adc1a150874fa1a166c8fc65fd05
AdrS <AdrS1118@gmail.com>
2015-03-04 21:53:37 -0500

    added argument parsing + code to find right coefitients for system

--- polyReg.py
+++ polyReg.py
@@ -1,3 +1,4 @@
+import sys
 def readPoints(filePath):
 	'''reads in list of points from file with format x1, y2\\nx2, y2 ...'''
 	points = []
@@ -25,6 +26,35 @@ def getLValues(points, degree):
 			lsum = lsum + (x**n) * y
 		values.append(lsum)
 	return values
-
+def getSumsOfPowersOfXCords(points, degree):
+	values = []
+	for i in range(0, degree * degree + 1):
+		rsum = 0.0
+		for x, y in points:
+			rsum = rsum + x**i
+		values.append(rsum)
+	return values
+def getRValues(points, degree):
+	powers = getSumsOfPowersOfXCords(points, degree)
+	values = []
+	for i in range(0, degree + 1):
+		row = []
+		for j in range(0, degree + 1):
+			row.append(powers[i + j])
+		values.append(row)
+	return values
+def usage():
+	print "python2 polyReg.py data.csv <degree>"
+	print "where data.csv list x,y pairs and degree is a natural number"
 if __name__ == '__main__':
-	pass
+	if len(sys.argv) != 3:
+		usage()
+		exit(2)
+	if not sys.argv[2].isdigit():
+		print "error: second parameter should be a positive integer"
+		exit(1)
+	degree = int(sys.argv[2])
+	pts = readPoints(sys.argv[1])
+	print getLValues(pts, degree)
+	print getRValues(pts, degree)
+	
